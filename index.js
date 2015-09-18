3f7392b0f4b0b597360e4b75b6ae597a84977aee
Mathieu Maes <mathieu@zimmo.be>
2015-09-18 16:18:31 +0200

    Implement template extending (WIP)

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 var Twig = require("twig");
+var _ = require("underscore");
+var path = require("path");
 var loaderUtils = require("loader-utils");
 
 Twig.extend(function(Twig) {
     var compiler = Twig.compiler;
 
     compiler.module['webpack'] = function(id, tokens, pathToTwig) {
+        id = path.basename(id, ".twig");
+        var includes = [];
+        _.each(JSON.parse(tokens), function(token) {
+            if (token.type == "logic" && token.token.type) {
+                switch(token.token.type) {
+                    case 'Twig.logic.type.extends':
+                        _.each(token.token.stack, function(token) {
+                            includes.push("./" + token.value + ".twig");
+                        });
+                        break;
+                    case 'Twig.logic.type.import':
+                        if (token.token.expression != '_self') {
+                            throw new Error("Twig-loader: Importing macro's is not yet supported");
+                            //console.dir(token.token);
+                        }
+                        break;
+                    case 'Twig.logic.type.include':
+                        throw new Error("Twig-loader: Including templates is not yet supported");
+                    //console.dir(token.token.stack);
+                    //break;
+                }
+            }
+        });
+
         var output = [
             '    var twig = require("' + pathToTwig + '").twig,',
-            '        template = ' + compiler.wrap(id, tokens) + ';\n',
+            '        template = twig({id:"'+id +'", data:'+tokens+', allowInlineIncludes: true});\n',
 //            '    template.options.autoescape = ' + moduleConfig.autoescape + ';\n',
             '    module.exports = function(context) { return template.render(context); }'
         ];
+        if (includes.length > 0) {
+            var includesOutput = "require("+ JSON.stringify(includes) +");";
+            output.unshift(includesOutput);
+        }
+        //console.log(output.join('\n'));
 
         return output.join('\n');
     };
